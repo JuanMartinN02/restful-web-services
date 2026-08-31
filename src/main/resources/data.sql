7325d00987c283454d7f6c12c878a6fb4e5379b9
JuanMartinN02 <juannogueiram@gmail.com>
2026-08-31 16:09:15 -0400

    157. Step 28 - Creating User Entity and some test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(id,birth_date,name)
+values (1001, current_date(),'Jose');
+
+insert into user_details(id,birth_date,name)
+values (1002, current_date(),'Pascual');
+
+insert into user_details(id,birth_date,name)
+values (1003, current_date(),'Pedro');
+
+insert into user_details(id,birth_date,name)
+values (1004, current_date(),'Jayme');
